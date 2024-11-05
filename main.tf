f3713ba4c69593ad03725be13ec5798c7d8b1956
testuser <testuser@gmail.com>
2024-11-05 12:16:38 +0530

    modified vpc property as true in main.tffile

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 resource "aws_vpc" "terraform_test_vpc" {
   cidr_block         = "172.31.0.0/16"
   enable_dns_hostnames = true
-  enable_dns_support   = false
+  enable_dns_support   = true
  
   tags = {
     Name = "terraform_tf_test_vpc"
